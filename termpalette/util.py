76e0cb0571b5ef9da983786f0cf7e816eb9aee74
Stefan Seemayer <stefan@seemayer.de>
2013-01-08 15:17:47 +0100

    Add color enumeration

--- termpalette/util.py
+++ termpalette/util.py
@@ -1,6 +1,8 @@
 import itertools
 
-
+def enum(*sequential, **named):
+	enums = dict(zip(sequential, range(len(sequential))), **named)
+	return type('Enum', (), enums)
 
 def color_command(*argv):
 	return "\33[" + ";".join( str(a) for a in argv ) + "m"
@@ -17,9 +19,17 @@ def bg(*argv):
 	else:
 		return [40 + a for a in argv]
 
-def bold(*argv):
+def bold():
 	return 1
 
+def italic():
+	return 3
+
+def underline():
+	return 4
+
+
+colors = enum('BLACK', 'RED', 'GREEN', 'YELLOW', 'BLUE', 'MAGENTA', 'CYAN', 'WHITE', 'DEFAULT')
 
 colornames_long  = ['black', 'red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white', 'default']
 colornames_short = ['blk', 'red', 'grn', 'yel', 'blu', 'mag', 'cya', 'wht', 'def']
