595eb70d67f57b672293b4dd4cc01d85828969d3
Stefan Seemayer <stefan@seemayer.de>
2013-01-08 15:37:17 +0100

    Add brightness comparison

---
+++ termpalette/viewers/brightness.py
@@ -0,0 +1,23 @@
+from termpalette.viewers import viewer
+from termpalette.util import *
+import sys
+
+@viewer('brightness', 'A color palette comparing color brightnesses')
+def brightness():
+
+	sys.stdout.write("\n")
+	sys.stdout.write( " " * 7 + " ".join( " {} ".format(color) for color in colornames_short) + "\n" )
+
+	for bold in range(2):
+
+		sys.stdout.write("bright " if bold else "dark   ")
+
+		for bg_color, bg_name in enumerate(colornames_short):
+
+			if bold:
+				sys.stdout.write( color_command( 7, bold, fg(bg_color) ) + " " * 5 + color_command() + " ")
+			else: 
+				sys.stdout.write( color_command( 7, bold, bg(bg_color) ) + " " * 5 + color_command() + " ")
+
+		sys.stdout.write("\n")
+	sys.stdout.write("\n")
