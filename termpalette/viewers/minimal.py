96d41aad8d29272137dba63a91becfee8a90f486
Stefan Seemayer <stefan@seemayer.de>
2013-01-08 15:48:12 +0100

    Made minimal viewer parametric (cell content, color label style)

--- termpalette/viewers/minimal.py
+++ termpalette/viewers/minimal.py
@@ -3,13 +3,16 @@ from termpalette.util import *
 import sys
 
 @viewer('minimal', 'A minimal matrix of colors')
-def minimal():
+def minimal(cell=" ☻ ", labels=colornames_short):
 
-	sys.stdout.write( " " * 4 + " ".join(colornames_short) + "\n" )
-	for fg_color, fg_name in enumerate(colornames_short):
-		sys.stdout.write(fg_name + " ")
-		for bg_color in range(len(colornames_short)):
-			sys.stdout.write( color_command( bg(bg_color), fg(fg_color) ) + " ☺ " + color_command() + " ")
+	labellen = max( len(cn) for cn in labels )
+	cellsize = max( labellen, len(cell) )
+
+	sys.stdout.write( " " * (labellen+1) + " ".join( "{:^{}}".format( cn, cellsize) for cn in labels ) + "\n" )
+	for fg_color, fg_name in enumerate(labels):
+		sys.stdout.write("{:>{}} ".format(fg_name, labellen))
+		for bg_color in range(len(labels)):
+			sys.stdout.write( color_command( bg(bg_color), fg(fg_color) ) + "{:^{}}".format(cell, cellsize) + color_command() + " ")
 
 		sys.stdout.write("\n")
 	sys.stdout.write("\n")
